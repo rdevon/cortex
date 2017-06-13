dc6940e5e1bc75639a4d31a561b4605e94ebb035
srinjaypaul <srinjaypaul@gmail.com>
2017-06-13 12:02:24 -0600

    Code modularized

--- tensorflow_tutorials/convolutional_autoencoder/CAE.py
+++ tensorflow_tutorials/convolutional_autoencoder/CAE.py
@@ -12,7 +12,7 @@ def autoencoder(input_shape, n_filters, filter_sizes, t):
 	t: input format specifier, number of values per pixel in the input.
 	samples:
 	mnist: input_shape=[None, 784], n_filters=[10, 10, 10], filter_sizes=[3, 3, 3], t=1
-	cifar10: input_shape=[None, 1024], n_filters=[10, 10, 10], filter_sizes=[3, 3, 3], t=1
+	cifar10: input_shape=[None, 900], n_filters=[10, 10, 10], filter_sizes=[3, 3, 3], t=1
 	
 	Output Parameters:
 	x: input tensor
@@ -21,6 +21,17 @@ def autoencoder(input_shape, n_filters, filter_sizes, t):
 	cost: loss after regeneration
 	summary_op: tensorboard summary variable for tracking cost	
 	"""
+	
+	temp = encoder(input_shape, n_filters, filter_sizes, t)
+	temp1 = decoder(temp['x'], temp['z'], temp['enc'], temp['shapes'], temp['x_tensor'])
+	
+	return {'x': temp['x'], 'z': temp['z'], 'y': temp1['y'], 'cost': temp1['cost'], 'summary_op': temp1['summary_op']}
+	
+	
+	
+	
+def encoder(input_shape, n_filters, filter_sizes, t):
+
 	x = tf.placeholder(tf.float32, input_shape, name='x')
 	# ensure 2D tensor
 	if len(x.get_shape()) == 2:
@@ -35,7 +46,8 @@ def autoencoder(input_shape, n_filters, filter_sizes, t):
 	else:
 		raise ValueError('Unsupported input dimensions')
 	current_input = x_tensor
-	encoder = []
+	
+	enc = []	
 	shapes = []
 	#create the structure
 	for layer_i, n_output in enumerate(n_filters):  
@@ -43,20 +55,28 @@ def autoencoder(input_shape, n_filters, filter_sizes, t):
 		shapes.append(current_input.get_shape().as_list())
 		W = tf.Variable(tf.random_uniform([filter_sizes[layer_i], filter_sizes[layer_i], n_input, n_output], -1.0 / math.sqrt(n_input), 1.0 / math.sqrt(n_input)))
 		b = tf.Variable(tf.zeros([n_output]))
-		encoder.append(W)                  
+		enc.append(W)                  
 		x1 = tf.add(tf.nn.conv2d(current_input, W, strides=[1, 2, 2, 1], padding='SAME'), b)
 		f1 = 0.5 * (1 + 0.2)
 		f2 = 0.5 * (1 - 0.2)
 		output = f1 * x1 + f2 * abs(x1)
 		current_input = output
 	
-	z = current_input #output after phase 1, compression
 	
-	encoder.reverse() #start reverse process, regeneration
+	z = current_input #output after phase 1, compression
+	enc.reverse()
 	shapes.reverse()
 	
+	return{'x': x, 'z': z, 'enc': enc, 'shapes': shapes, 'x_tensor': x_tensor}
+	
+
+def decoder(x, z, enc, shapes, x_tensor):
+	
+	
+	current_input = z
+	
 	for layer_i, shape in enumerate(shapes):
-		W = encoder[layer_i]
+		W = enc[layer_i]
 		b = tf.Variable(tf.zeros([W.get_shape().as_list()[2]]))
 		x2 = tf.add(tf.nn.conv2d_transpose(current_input, W, tf.stack([tf.shape(x)[0], shape[1], shape[2], shape[3]]), strides=[1, 2, 2, 1], padding='SAME'), b)
 		f1 = 0.5 * (1 + 0.2)
@@ -66,10 +86,10 @@ def autoencoder(input_shape, n_filters, filter_sizes, t):
 		
 	y = current_input # regenerated inputs after phase 2
 	
-	with tf.name_scope('cost'):  # track cost for tensorboard
+	with tf.name_scope('cost'):  # track cost for tensorboard		
 		cost = tf.reduce_sum(tf.square(y - x_tensor))
 		
-	tf.summary.scalar("cost", cost)
+	tf.summary.scalar("cost", cost)	
 	summary_op = tf.summary.merge_all()
 	
-	return {'x': x, 'z': z, 'y': y, 'cost': cost, 'summary_op': summary_op}
+	return{'y' : y, 'cost': cost, 'summary_op': summary_op}
--- tensorflow_tutorials/convolutional_autoencoder/VCAE.py
+++ tensorflow_tutorials/convolutional_autoencoder/VCAE.py
@@ -17,6 +17,13 @@ def autoencoder(input_shape, padding, stride):
 	y: regenrerated output
 	cost: loss after regeneration
 	'''
+	temp = encoder(input_shape, padding, stride)
+	temp1 = decoder(temp['x'], temp['z'], temp['x_tensor'], input_shape, padding, stride)
+	return {'x': temp['x'], 'z': temp['z'], 'y': temp1['y'], 'cost': temp1['cost']}
+	
+	
+def encoder(input_shape, padding, stride):
+	
 	x = tf.placeholder(tf.float32, input_shape)
 	
 	if len(x.get_shape()) == 5:
@@ -25,6 +32,7 @@ def autoencoder(input_shape, padding, stride):
 		raise ValueError('Unsupported input dimensions')
 		
 	curr = x_tensor
+	
 	n_input = curr.get_shape().as_list()[3]
 	enc_W = tf.Variable(tf.random_uniform([3, 3, 3, n_input, 10], -1.0 / math.sqrt(n_input), 1.0 / math.sqrt(n_input)))
 	curr = tf.nn.conv3d(curr, enc_W, strides=stride, padding=padding)
@@ -38,10 +46,14 @@ def autoencoder(input_shape, padding, stride):
 	
 	z = curr
 	
-	n_input = curr.get_shape().as_list()[3]
+	return {'x': x, 'z': z, 'x_tensor': x_tensor}
+	
+def decoder(x, z, x_tensor, input_shape, padding, stride):
+
+	n_input = z.get_shape().as_list()[3]
 	dec_W = tf.Variable(tf.random_uniform([3, 3, 3, n_input, 10], -1.0 / math.sqrt(n_input), 1.0 / math.sqrt(n_input)))
 	
-	curr = tf.nn.conv3d_transpose(curr, dec_W, output_shape=input_shape, strides=stride, padding=padding)
+	curr = tf.nn.conv3d_transpose(z, dec_W, output_shape=input_shape, strides=stride, padding=padding)
 	b_shape = curr.get_shape().as_list()[1:]
 	dec_b = tf.Variable(tf.fill(b_shape, 0.01), name="dec_b")
 	curr += dec_b
@@ -55,4 +67,4 @@ def autoencoder(input_shape, padding, stride):
 	
 	cost = tf.reduce_sum(tf.square(y - x_tensor))
 	
-	return {'x': x, 'z': z, 'y': y, 'cost': cost}
+	return {'y': y, 'cost': cost}
