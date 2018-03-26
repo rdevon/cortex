b26bc98006fc2e9473a4ea44aac3ec5643ac8d32
bbradt <BBradT@gmail.com>
2018-03-26 14:48:18 -0600

    overhaul of regularizer to fit cortex formatting+args

--- models/modules/regularization.py
+++ models/modules/regularization.py
@@ -5,43 +5,78 @@ for various loss-types.
 TODO L1 with truncation
 
 '''
-import torch.nn as nn
-import torch.nn.functional as F
 import torch
+import torch.nn as nn
+
 
 def spectral_norm(input):
-   '''
-       input : a pytorch tensor, for now, assuming real input
-
-       output : the spectral norm of the tensor
-   '''
-   return torch.sqrt(torch.max(torch.eig(torch.mul(torch.t(input), input))))
-   
-
-
-def reg_term(input, reg="l1", factor=0.0005, **kwargs):
     '''
-        reg - computes standard regularization terms
-        
-        Args:
-            input: input tensor (N, *)
-            target: target tensor (N, *)
-
-        Output: scalar, if reduce is false
-
-        KWArgs:
-            reg: type of regularization: l1, l2, en, or spectral
-            factor: \lambda regularization hyper-parameter
-            kwargs: <see the nn.L1Loss docstring>
+        input : a pytorch tensor, for now, assuming real input
+        output : the spectral norm of the tensor, a functional
     '''
-    reg = reg.lower()
-    reg_loss = 0
-    if reg == "l1" or reg == "en":
-        l1_loss = nn.L1Loss(**kwargs)
-        reg_loss += factor*l1_loss(input)
-    if reg == "l2" or reg == "en":
-        l2_loss = nn.MSELoss(**kwargs)
-        reg_loss += factor*l2_loss(input)
-    if reg == "spec":
-        reg_loss += factor*spectral_norm(input)
-    return reg_loss
+    return torch.sqrt(torch.max(torch.eig(torch.mul(torch.t(input), input))))
+
+
+def l1_norm(input):
+        '''
+            input: a pytorch tensor
+            output: l1 norm functional
+        '''
+        return input.norm(1)
+
+
+def l2_norm(input):
+    '''
+        input: a pytorch tensor
+        output: l2 norm functional
+    '''
+    return input.norm(2)
+
+
+def elastic_net(input):
+    '''
+        input: a pytorch tensor
+        output: elastic net norm functional
+    '''
+    return l1_norm(input) + l2_norm(input)
+
+
+class Regularizer():
+    '''
+        Class for performing regularization on parameters
+    '''
+    REGULARIZERS = {'l1': l1_norm, 'l2':l2_norm, 'en':elastic_net, 'sp':spectral_norm}
+    regularizer = None
+    factor = 0.0005
+    loss = None
+
+    def __init__(self, reg_type='l1', factor=0.0005, loss=nn.CrossEntropyLoss()):
+        '''
+           reg_type - the kind of regularization
+                  l1 - l1 regularization
+                  l2 - l2 regularization
+                  en - elastic net regularization
+                  sp - spectral regularization
+
+          factor - the regularization hyper parameter
+          
+          loss - the kind of loss to regularize
+
+          methods: compute_reg_term - see docstring below
+        '''
+        self.regularizer = self.REGULARIZERS[reg_type]
+        self.factor=factor
+        self.loss = loss
+
+    def compute_reg_term(self, input, target, parameters):
+        '''
+            computes the regularization term for given regularization
+                input: a torch tensor of inputs
+                target: a torch tensor of targets
+                parameters: the parameters to regularize
+        '''
+        total_loss = self.loss(input, target)
+        for beta in parameters:
+            total_loss += self.factor*self.regularizer(beta)
+        return total_loss
+
--- models/reg_classifier.py
+++ models/reg_classifier.py
@@ -10,9 +10,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from .modules.convnets import SimpleConvEncoder as Net
-from .modules.regularization import reg_term
+from .modules.regularization import Regularizer
 
 logger = logging.getLogger('cortex.models' + __name__)
+
+regularizer_args_ = dict(reg_type='l1', factor=0.01)
 
 classifier_args_ = dict(dim_h=256, batch_norm=True, dropout=0.5, nonlinearity='ReLU',
                         f_size=4, stride=2, pad=1, min_dim=4)
@@ -24,32 +26,33 @@ DEFAULTS = dict(
         learning_rate=1e-4,
     ),
     model=dict(),
-    procedures=dict(criterion=nn.CrossEntropyLoss()),
+    procedures=dict(criterion=nn.CrossEntropyLoss(), regularizer='l1'),
     train=dict(
         epochs=200,
         summary_updates=100,
         archive_every=10
     )
+    
 )
 
 
-def classify(nets, inputs, criterion=None, regularizer=reg_term):
+def classify(nets, inputs, criterion=None, regularizer=None, factor=0.0005):
     net = nets['classifier']
     images = inputs['images']
     targets = inputs['targets']
+    reg_term = Regularizer(loss=criterion, reg_type=regularizer, factor=factor)
     outputs = net(images, nonlinearity=F.log_softmax)
-    loss = criterion(outputs, targets)
-    loss += regularizer(outputs)
+    loss = reg_term.compute_reg_term(outputs, targets, net.parameters())
     _, predicted = torch.max(outputs.data, 1)
     correct = 100. * predicted.eq(targets.data).cpu().sum() / targets.size(0)
     return loss, dict(loss=loss.data[0], accuracy=correct), None, 'accuracy'
 
 
-def build_model(data_handler, classifier_args=None):
+def build_model(data_handler, classifier_args=None, regularizer_args=None):
+    global REGULARIZER
     classifier_args = classifier_args or {}
     args = classifier_args_
     args.update(**classifier_args)
-
     shape = data_handler.get_dims('x', 'y', 'c')
     dim_l = data_handler.get_dims('labels')[0]
     net = Net(shape, dim_out=dim_l, **args)
