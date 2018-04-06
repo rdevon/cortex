9f8945d82dc1d3a9f265b68de7274bf752c91a85
Bradley Baker <bbradt@gmail.com>
2018-04-06 16:50:03 -0600

    CAE

--- models/modules/regularization.py
+++ models/modules/regularization.py
@@ -1,5 +1,5 @@
 '''
-Implements L1,  L2, Elastic-Net and Spectral Regularization
+Implements L1,  L2, Elastic-Net, Spectral Regularization, and Contractive Loss
 for various loss-types.
 
 TODO L1 with truncation
@@ -7,18 +7,50 @@ TODO L1 with truncation
 '''
 import torch
 import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+
+
+def contractive_loss(parameters, hidden_u):
+    """Compute the Contractive AutoEncoder Loss
+        input  `W` (FloatTensor): (N_hidden x N), where N_hidden and N are the
+          dimensions of the hidden units and input respectively.
+
+        `h` (Variable): the hidden units of the network, with dims
+          batch_size x N_hidden\
+
+         based on
+         https://github.com/avijit9/Contractive_Autoencoder_in_Pytorch/
+    """
+    dh = hidden_u * (1 - hidden_u)  # Hadamard product
+    # Sum through the input dimension to improve efficiency, as suggested in #1
+    w_sum = torch.sum(Variable(parameters)**2, dim=1)
+    # unsqueeze to avoid issues with torch.mv
+    w_sum = w_sum.unsqueeze(1)  # shape N_hidden x 1
+    return torch.sum(torch.mm(dh**2, w_sum), 0)
 
 
 def spectral_norm(input):
     '''
         input : a pytorch tensor, for now, assuming real input
-        output : the spectral norm of the tensor, a functional
-        is currently broken for general tensors
+        output : the spectral norm of the tensor, taken by partitions
 
         TODO: fix for general tensors
     '''
-    x = input.view(input.numel())
-    return torch.sqrt(torch.max(torch.eig(torch.mul(torch.t(x), x))))
+    x = input
+    print(x.size())
+    if len(x.size()) == 4: # Then it's a convolutional layer
+        x = x.transpose(0, 1)
+        x = x.contiguous()
+        x = x.view(x.size(0), x.size(1)*x.size(2)*x.size(3))
+    elif len(x.size()) > 2:
+        x = x.view(x.size(0), np.prod(x.size()[1:]))
+    elif len(x.size()) == 1:
+        x = x.view(1, x.size(0)) 
+    ex = torch.eig(x.t()*x)
+    ex = ex[:][0]
+    print(torch.max(ex))
+    return torch.max(ex)
 
 
 def l1_norm(input):
@@ -49,12 +81,14 @@ class Regularizer():
     '''
         Class for performing regularization on parameters
     '''
-    REGULARIZERS = {'l1': l1_norm, 'l2':l2_norm, 'en':elastic_net, 'sp':spectral_norm}
+    REGULARIZERS = {'l1': l1_norm, 'l2': l2_norm, 'en': elastic_net,
+                    'sp': spectral_norm, 'cl': contractive_loss}
     regularizer = None
     factor = 0.0005
     loss = None
 
-    def __init__(self, reg_type='l1', factor=0.0005, loss=nn.CrossEntropyLoss()):
+    def __init__(self, reg_type='l1', factor=0.0005,
+                 loss=nn.CrossEntropyLoss()):
         '''
            reg_type - the kind of regularization
                   l1 - l1 regularization
@@ -63,16 +97,15 @@ class Regularizer():
                   sp - spectral regularization
 
           factor - the regularization hyper parameter
-          
           loss - the kind of loss to regularize
-
           methods: compute_reg_term - see docstring below
         '''
+        self.reg_type = reg_type
         self.regularizer = self.REGULARIZERS[reg_type]
-        self.factor=factor
+        self.factor = factor
         self.loss = loss
 
-    def compute_reg_term(self, input, target, parameters):
+    def compute_reg_term(self, input, target, parameters, hidden_units=None):
         '''
             computes the regularization term for given regularization
                 input: a torch tensor of inputs
@@ -80,7 +113,9 @@ class Regularizer():
                 parameters: the parameters to regularize
         '''
         total_loss = self.loss(input, target)
+        if self.reg_type == 'cl':
+            total_loss += self.factor*self.regularizer(parameters,
+                                                       hidden_units)
         for beta in parameters:
             total_loss += self.factor*self.regularizer(beta)
         return total_loss
-
