4deb7f306eaa25b266882b084b4bb48df8191cac
devon <erroneus@gmail.com>
2018-04-21 12:12:37 -0400

    Organization changes in arch
    Moved some functions to vae and immporting from there (autoencoder stuff).
    Changed densenet module to fully_connected

---
+++ arch/modules/fully_connected.py
@@ -0,0 +1,88 @@
+'''Simple dense network encoders
+
+'''
+
+import logging
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+logger = logging.getLogger('cortex.arch' + __name__)
+
+
+class LayerNorm(nn.Module):
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+
+
+class FullyConnectedNet(nn.Module):
+    def __init__(self, dim_in, dim_out=None, dim_h=64, batch_norm=True, layer_norm=False,
+                 dropout=False, nonlinearity='ReLU', n_levels=None):
+        super(FullyConnectedNet, self).__init__()
+        models = nn.Sequential()
+
+        dim_out_ = dim_out
+
+        if isinstance(dim_h, (list, tuple)):
+            pass
+        elif n_levels:
+            dim_h = [dim_h for _ in xrange(n_levels)]
+        else:
+            dim_h = [dim_h]
+
+        if not nonlinearity:
+            pass
+        elif hasattr(nn, nonlinearity):
+            nonlin = getattr(nn, nonlinearity)
+            if nonlinearity == 'LeakyReLU':
+                nonlinearity = nonlin(0.2, inplace=True)
+            else:
+                nonlinearity = nonlin()
+        else:
+            raise ValueError(nonlinearity)
+
+        dim_out = dim_in
+
+        for i, dim_h in enumerate(dim_h):
+            dim_in = dim_out
+            dim_out = dim_h
+            name = 'dense_({}/{})_{}'.format(dim_in, dim_out, i + 1)
+            models.add_module(name, nn.Linear(dim_in, dim_out))
+            if dropout:
+                models.add_module(name + '_do', nn.Dropout(p=dropout))
+            if layer_norm:
+                models.add_module(name + '_ln', LayerNorm(dim_out))
+            elif batch_norm:
+                models.add_module(name + '_bn', nn.BatchNorm1d(dim_out))
+            if nonlinearity:
+                models.add_module('{}_{}'.format(name, nonlin), nonlinearity)
+
+        if dim_out_:
+            name = 'dense_({}/{})_{}'.format(dim_out, dim_out_, 'final')
+            models.add_module(name, nn.Linear(dim_out, dim_out_))
+
+        self.models = models
+
+    def forward(self, x, nonlinearity=None, **nonlinearity_args):
+        nonlinearity_args = nonlinearity_args or {}
+        x = self.models(x)
+
+        if nonlinearity:
+            if callable(nonlinearity):
+                x = nonlinearity(x, **nonlinearity_args)
+            elif hasattr(F, nonlinearity):
+                x = getattr(F, nonlinearity)(x, **nonlinearity_args)
+            else:
+                raise ValueError()
+        return x
--- arch/vae.py
+++ arch/vae.py
@@ -8,16 +8,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from .modules.densenet import FullyConnectedNet
+from .modules.fully_connected import FullyConnectedNet
 from .classifier import classify
 from .utils import cross_correlation
 
 
 logger = logging.getLogger('cortex.arch' + __name__)
 
-mnist_encoder_args_ = dict(dim_h=64, batch_norm=True, f_size=5, pad=2, stride=2, min_dim=7, dim_out=1028)
+resnet_encoder_args_ = dict(dim_h=64, batch_norm=True, f_size=3, n_steps=3)
+resnet_decoder_args_ = dict(dim_h=64, batch_norm=True, f_size=3, n_steps=3)
+mnist_encoder_args_ = dict(dim_h=64, batch_norm=True, f_size=5, pad=2, stride=2, min_dim=7)
 mnist_decoder_args_ = dict(dim_h=64, batch_norm=True, f_size=4, pad=1, stride=2, n_steps=2)
-convnet_encoder_args_ = dict(dim_h=64, batch_norm=True, n_steps=3, dim_out=1028)
+convnet_encoder_args_ = dict(dim_h=64, batch_norm=True, n_steps=3)
 convnet_decoder_args_ = dict(dim_h=64, batch_norm=True, n_steps=3)
 
 
@@ -80,36 +82,87 @@ def classifier_routine(data, models, losses, results, viz, **kwargs):
     classify(classifier, vae_net.mu, Y, losses=losses, results=results, **kwargs)
 
 
-def build_model(data, models, model_type='convnet', dim_z=64, encoder_args=None, decoder_args=None):
+def update_encoder_args(x_shape, model_type='convnet', encoder_args=None):
     encoder_args = encoder_args or {}
-    decoder_args = decoder_args or {}
-    shape = data.get_dims('x', 'y', 'c')
-    dim_l = data.get_dims('labels')
 
-    if model_type == 'convnet':
+    if model_type == 'resnet':
+        from .modules.resnets import ResEncoder as Encoder
+        encoder_args_ = resnet_encoder_args_
+    elif model_type == 'convnet':
         from .modules.convnets import SimpleConvEncoder as Encoder
-        from .modules.conv_decoders import SimpleConvDecoder as Decoder
         encoder_args_ = convnet_encoder_args_
-        decoder_args_ = convnet_decoder_args_
     elif model_type == 'mnist':
         from .modules.convnets import SimpleConvEncoder as Encoder
-        from .modules.conv_decoders import SimpleConvDecoder as Decoder
         encoder_args_ = mnist_encoder_args_
-        decoder_args_ = mnist_decoder_args_
     else:
         raise NotImplementedError(model_type)
 
     encoder_args_.update(**encoder_args)
-    decoder_args_.update(**decoder_args)
-    if shape[0] == 64:
+    if x_shape[0] == 64:
         encoder_args_['n_steps'] = 4
+    elif x_shape[0] == 128:
+        encoder_args_['n_steps'] = 5
+
+    return Encoder, encoder_args_
+
+
+def update_decoder_args(x_shape, model_type='convnet', decoder_args=None):
+    decoder_args = decoder_args or {}
+
+    if model_type == 'resnet':
+        from .modules.resnets import ResDecoder as Decoder
+        decoder_args_ = resnet_decoder_args_
+    elif model_type == 'convnet':
+        from .modules.conv_decoders import SimpleConvDecoder as Decoder
+        decoder_args_ = convnet_decoder_args_
+    elif model_type == 'mnist':
+        from .modules.conv_decoders import SimpleConvDecoder as Decoder
+        decoder_args_ = mnist_decoder_args_
+    else:
+        raise NotImplementedError(model_type)
+
+    decoder_args_.update(**decoder_args)
+    if x_shape[0] == 64:
         decoder_args_['n_steps'] = 4
+    elif x_shape[0] == 128:
+        decoder_args_['n_steps'] = 5
 
-    encoder = Encoder(shape, **encoder_args_)
-    decoder = Decoder(shape, dim_in=dim_z, **decoder_args_)
-    vae = VAE(encoder, decoder, dim_out=encoder_args_['dim_out'], dim_z=dim_z)
+    return Decoder, decoder_args_
+
+
+def build_encoder(models, x_shape, dim_out, Encoder, key='encoder', **encoder_args):
+    logger.debug('Forming encoder with class {} and args: {}'.format(Encoder, encoder_args))
+    encoder = Encoder(x_shape, dim_out=dim_out, **encoder_args)
+
+    if models is not None:
+        models[key] = encoder
+
+    return encoder
+
+
+def build_decoder(models, x_shape, dim_in, Decoder, key='decoder', **decoder_args):
+    logger.debug('Forming dencoder with class {} and args: {}'.format(Decoder, decoder_args))
+    decoder = Decoder(x_shape, dim_in=dim_in, **decoder_args)
+
+    if models is not None:
+        models[key] = decoder
+
+    return decoder
+
+
+def build_model(data, models, model_type='convnet', dim_z=64, dim_encoder_out=1028, encoder_args=None,
+                decoder_args=None):
+    x_shape = data.get_dims('x', 'y', 'c')
+    dim_l = data.get_dims('labels')
+
+    Encoder, encoder_args = update_encoder_args(x_shape, model_type=model_type, encoder_args=encoder_args)
+    Decoder, decoder_args = update_decoder_args(x_shape, model_type=model_type, decoder_args=decoder_args)
+
+    encoder = build_encoder(None, x_shape, dim_encoder_out, Encoder, **encoder_args)
+    decoder = build_decoder(None, x_shape, dim_z, Decoder, **decoder_args)
+    vae = VAE(encoder, decoder, dim_out=dim_encoder_out, dim_z=dim_z)
+
     classifier = FullyConnectedNet(dim_z, dim_h=[64, 64], dim_out=dim_l, batch_norm=True, dropout=0.2)
-
     models.update(vae=vae, classifier=classifier)
 
 
@@ -122,7 +175,7 @@ DEFAULT_CONFIG = dict(
         optimizer='Adam',
         learning_rate=1e-4,
     ),
-    model=dict(dim_z=64, encoder_args=None, decoder_args=None),
+    model=dict(dim_z=64, dim_encoder_out=1028, encoder_args=None, decoder_args=None),
     routines=dict(vae=dict(criterion=F.mse_loss, beta_kld=1.),
                   classifier=dict(criterion=nn.CrossEntropyLoss())),
     train=dict(
