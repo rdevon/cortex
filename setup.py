7139f034d4278b1f83d1b4b271bcb36284a9ca08
joeljpoulin <joel.poulin@polymtl.ca>
2018-06-24 21:27:55 -0400

    Change requirements location for rdt testing.

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from setuptools import setup
 packages = [
     'cortex', 'cortex._lib', 'cortex.built_ins', 'cortex._lib.data',
     'cortex.built_ins.datasets', 'cortex.built_ins.models',
-    'cortex.built_ins.networks', 'cortex.built_ins.transforms']
+    'cortex.built_ins.networks', 'cortex.built_ins.transforms'
+]
 
-setup(name='cortex',
-      version='0.1',
-      description='A library for wrapping your pytorch code',
-      author='R Devon Hjelm',
-      author_email='erroneus@gmail.com',
-      packages=packages,
-      install_requires=[
-          'imageio', 'matplotlib', 'progressbar2', 'scipy', 'sklearn',
-          'torchvision', 'visdom', 'pyyaml'],
-      entry_points={
-          'console_scripts': [
-              'cortex=cortex.main:main']
-      },
-      zip_safe=False)
+install_requirements = [
+    'imageio', 'torch', 'imageio', 'matplotlib', 'progressbar2', 'scipy',
+    'sklearn', 'torchvision', 'visdom', 'pyyaml', 'pathlib'
+]
+
+setup(
+    name='cortex',
+    version='0.1',
+    description='A library for wrapping your pytorch code',
+    author='R Devon Hjelm',
+    author_email='erroneus@gmail.com',
+    packages=packages,
+    install_requires=install_requirements,
+    entry_points={'console_scripts': ['cortex=cortex.main:main']},
+    zip_safe=False)
