d93d456ac08cc3fa12efe9b670f3e89b75d04e8f
devon <erroneus@gmail.com>
2018-06-28 18:45:58 -0400

    mine runs

--- cortex/built_ins/models/__init__.py
+++ cortex/built_ins/models/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['adversarial_autoencoder', 'ali', 'classifier', 'gan', 'vae']
+__all__ = ['adversarial_autoencoder', 'ali', 'classifier', 'gan', 'mine', 'vae']
--- cortex/built_ins/models/mine.py
+++ cortex/built_ins/models/mine.py
@@ -6,7 +6,7 @@
 from cortex.plugins import register_model
 from cortex.built_ins.models.ali import ALIDiscriminator
 from cortex.built_ins.models.gan import GAN, GradientPenalty
-
+from cortex.built_ins.models.vae import ImageEncoder
 
 
 class MINE(ALIDiscriminator):
@@ -18,7 +18,10 @@ class MINE(ALIDiscriminator):
 
     def __init__(self):
         super().__init__(contract=dict(nets=dict(discriminator='mine')))
-
+        contract = dict(nets=dict(network='mine'),
+                        kwargs=dict(penalty_type='mine_penalty_type',
+                                    penalty_amount='mine_penalty_amount'))
+        self.penalty = GradientPenalty(contract=contract)
 
     def routine(self, X, Z, X_m, Z_m, mine_measure='JSD'):
         '''
@@ -31,6 +34,7 @@ class MINE(ALIDiscriminator):
         '''
 
         super().routine(X, X_m, Z, Z_m, measure=mine_measure)
+        self.penalty.routine((X, Z))
 
 
 class GAN_MINE(GAN):
@@ -43,14 +47,33 @@ class GAN_MINE(GAN):
         super().__init__()
         self.mine = MINE()
 
-    def routine(self, X, Z, X_m, Z_m):
-        self.generator.routine(Z)
+        encoder_contract = dict(nets=dict(encoder='x_encoder'),
+                                kwargs=dict(dim_out='dim_int'))
+        self.encoder = ImageEncoder(contract=encoder_contract)
 
-    def train_step(self, generator_updates=1, discriminator_updates=1):
+    def build(self, noise_type='normal', dim_z=64):
+        super().build(noise_type=noise_type, dim_z=dim_z)
+        self.encoder.build()
+        self.mine.build()
+
+    def routine(self, Z, Z_m, mine_measure=None, beta=1.0):
         '''
 
         Args:
-            generator_updates: Number of generator updates per step.
+            beta: Factor for mutual information maximization for generator.
+
+        '''
+        self.generator.routine(Z)
+        X = self.generator.generate(Z)
+        E_pos, E_neg, _, _ = self.mine.score(X, X, Z, Z_m, mine_measure)
+
+        self.losses.generator += (E_neg - E_pos)
+
+    def train_step(self, mine_updates=1, discriminator_updates=1):
+        '''
+
+        Args:
+            mine_updates: Number of MINE updates per step.
             discriminator_updates: Number of discriminator updates per step.
 
         '''
@@ -63,23 +86,42 @@ class GAN_MINE(GAN):
             self.optimizer_step()
             self.penalty.train_step()
 
-        for _ in range(generator_updates):
-            self.generator.train_step()
+        Z_P = Z
+
+        for _ in range(mine_updates):
+            self.data.next()
+            Z = self.inputs('Z')
+            generated = self.generator.generate(Z)
+            self.mine.routine(generated, Z, generated, Z_P)
+            self.optimizer_step()
+
+        self.routine(Z, Z_P)
+        self.optimizer_step()
 
     def eval_step(self):
         self.data.next()
-
         inputs, Z = self.inputs('inputs', 'Z')
         generated = self.generator.generate(Z)
-        self.discriminator.routine(inputs, generated)
-        self.penalty.routine(auto_input=True)
-        self.generator.routine(auto_input=True)
+        self.discriminator.routine(inputs, generated.detach())
 
-    def visualize(self, images, Z):
+        Z_P = Z
+
+        self.data.next()
+        Z = self.inputs('Z')
+        generated = self.generator.generate(Z)
+        self.mine.routine(generated, Z, generated, Z_P)
+
+        self.routine(Z, Z_P)
+
+    def visualize(self, images, Z, targets):
         self.add_image(images, name='ground truth')
         generated = self.generator.generate(Z)
         self.discriminator.visualize(images, generated)
         self.generator.visualize(Z)
+        self.data.next()
+        Z_N = self.inputs('Z')
+
+        self.mine.visualize(generated, generated, Z, Z_N, targets)
 
 
 register_model(GAN_MINE)
