0c753a33ee9321c42c6cecadaaa181235518ada6
joeljpoulin <joel.poulin@polymtl.ca>
2018-07-06 13:10:28 -0400

    Tries new env file.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ packages = [
 ]
 
 install_requirements = [
-    'torch', 'imageio', 'matplotlib', 'progressbar2', 'scipy',
-    'sklearn', 'torchvision', 'visdom', 'pyyaml', 'pathlib',
+    'imageio', 'matplotlib', 'progressbar2', 'scipy',
+    'sklearn', 'visdom', 'pyyaml', 'pathlib',
     'sphinxcontrib-napoleon', 'nibabel'
 ]
 
