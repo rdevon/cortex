061cc687f79c002c9f603cf0629a70ad13fb3b31
joeljpoulin <joel.poulin@polymtl.ca>
2018-07-09 12:27:37 -0400

    Adds back torch and torchvision as setup.py requirements.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = [
 
 install_requirements = [
     'imageio', 'matplotlib', 'progressbar2', 'scipy', 'sklearn', 'visdom',
-    'pyyaml', 'pathlib', 'sphinxcontrib-napoleon', 'nibabel'
+    'pyyaml', 'pathlib', 'sphinxcontrib-napoleon', 'nibabel', 'torch', 'torchvision'
 ]
 
 setup(name='cortex',
