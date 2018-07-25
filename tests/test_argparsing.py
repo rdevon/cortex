34c39d8b5082ed9ffa1de2228c307886101e8b7e
joeljpoulin <joel.poulin@polymtl.ca>
2018-07-25 13:26:23 -0400

    flake8

--- tests/test_argparsing.py
+++ tests/test_argparsing.py
@@ -1,110 +1,86 @@
 from cortex.built_ins.models.classifier import ImageClassification
-from cortex._lib import (config, data, exp, optimizer, setup_cortex,
-                         setup_experiment, train)
+from cortex._lib import (config, data, optimizer, setup_experiment, train)
 from cortex._lib.utils import print_section
 from torch.nn import CrossEntropyLoss
 from argparse import Namespace
 import torch
 
-# args_namespace = Namespace(
-    #     classifier_args={'dropout': 0.2},
-    #     classifier_type='convnet',
-    #     clean=False,
-    #     command=None,
-    #     config_file=None,
-    #     criterion=nn.CrossEntropyLoss(),
-    #     device=0,
-    #     load_models=None,
-    #     meta=None,
-    #     name=None,
-    #     out_path=None,
-    #     reload=None,
-    #     reloads=None,
-    #     verbosity=1,
-    #     **{'data.batch_size': 128,
-    #        'data.copy_to_local': True,
-    #        'data.data_args': None,
-    #        'data.inputs': {'inputs':'images'},
-    #        'data.n_workers': 4,
-    #        'data.shuffle': True,
-    #        'data.skip_last_batch': False,
-    #        'data.source': 'CIFAR10',
-    #        'optimizer.clipping': None,
-    #        'optimizer.learning_rate': 0.001,
-    #        'optimizer.model_optimizer_options': None,
-    #        'optimizer.optimizer': 'Adam',
-    #        'optimizer.optimizer_options': None,
-    #        'optimizer.weight_decay': None,
-    #        'train.archive_every': 10,
-    #        'train.epochs': 1,
-    #        'train.eval_during_train': True,
-    #        'train.eval_only': False,
-    #        'train.quit_on_bad_values': True,
-    #        'train.save_on_best': 'losses.classifier',
-    #        'train.save_on_highest': None,
-    #        'train.save_on_lowest': None,
-    #        'train.test_mode': 'test',
-    #        'train.train_mode': 'train'}
-    # )
-
-# args = {'data':
-#     {
-#         'source': 'CIFAR10',
-#         'batch_size': 128,
-#         'n_workers': 4,
-#         'skip_last_batch': False,
-#         'inputs':
-#             {
-#                 'inputs': 'images'
-#             },
-#         'copy_to_local': True,
-#         'data_args': {},
-#         'shuffle': True
-#     },
-#     'model': {'classifier_type': 'convnet',
-#               'classifier_args': {'dropout': 0.2},
-#               'criterion': nn.CrossEntropyLoss()
-#               },
-#     'optimizer': {'optimizer': 'Adam',
-#                   'learning_rate': 0.001,
-#                   'weight_decay': {},
-#                   'clipping': {},
-#                   'optimizer_options': {},
-#                   'model_optimizer_options': {}
-#                   },
-#     'train': {'epochs': 200,
-#               'archive_every': 10,
-#               'quit_on_bad_values': True,
-#               'save_on_best': 'losses.classifier',
-#               'save_on_lowest': None,
-#               'save_on_highest': None,
-#               'eval_during_train': True,
-#               'train_mode': 'train',
-#               'test_mode': 'test',
-#               'eval_only': False
-#               },
-#     'classifier_args': {'dropout': 0.2},
-#     'classifier_type': 'convnet',
-#     'clean': False,
-#     'command': None,
-#     'config_file': None,
-#     'criterion': nn.CrossEntropyLoss(),
-#     'device': 0,
-#     'load_models': None,
-#     'meta': None,
-#     'name': None,
-#     'out_path': None,
-#     'reload': None,
-#     'reloads': None,
-#     'verbosity': 1,
-# }
-
-args = Namespace(classifier_args={'dropout': 0.2}, classifier_type='convnet', clean=False, command=None, config_file=None, criterion=CrossEntropyLoss(), device=0, load_models=None, meta=None, name=None, out_path=None, reload=None, reloads=None, verbosity=1, **{'data.batch_size': 128, 'data.copy_to_local': True, 'data.data_args': None, 'data.inputs': None, 'data.n_workers': 4, 'data.shuffle': True, 'data.skip_last_batch': False, 'data.source': 'CIFAR10', 'optimizer.clipping': None, 'optimizer.learning_rate': 0.001, 'optimizer.model_optimizer_options': None, 'optimizer.optimizer': 'Adam', 'optimizer.optimizer_options': None, 'optimizer.weight_decay': None, 'train.archive_every': 10, 'train.epochs': 1, 'train.eval_during_train': True, 'train.eval_only': False, 'train.quit_on_bad_values': True, 'train.save_on_best': 'losses.classifier', 'train.save_on_highest': None, 'train.save_on_lowest': None, 'train.test_mode': 'test', 'train.train_mode': 'train'})
-args_data = {'source': 'CIFAR10', 'batch_size': 128, 'n_workers': 4, 'skip_last_batch': False, 'inputs': {'inputs': 'images'},'copy_to_local': True, 'data_args': {}, 'shuffle': True}
-args_train = {'epochs': 1, 'archive_every': 10, 'quit_on_bad_values': True, 'save_on_best': 'losses.classifier', 'save_on_lowest': None, 'save_on_highest': None, 'eval_during_train': True, 'train_mode': 'train', 'test_mode': 'test', 'eval_only': False}
-args_optimizer = {'optimizer': 'Adam', 'learning_rate': 0.001, 'weight_decay': {}, 'clipping': {}, 'optimizer_options': {}, 'model_optimizer_options': {}}
 
 def test_argparsing():
+    args = Namespace(
+        classifier_args={'dropout': 0.2},
+        classifier_type='convnet',
+        clean=False,
+        command=None,
+        config_file=None,
+        criterion=CrossEntropyLoss(),
+        device=0,
+        load_models=None,
+        meta=None,
+        name=None,
+        out_path=None,
+        reload=None,
+        reloads=None,
+        verbosity=1,
+        **{
+            'data.batch_size': 128,
+            'data.copy_to_local': True,
+            'data.data_args': None,
+            'data.inputs': None,
+            'data.n_workers': 4,
+            'data.shuffle': True,
+            'data.skip_last_batch': False,
+            'data.source': 'CIFAR10',
+            'optimizer.clipping': None,
+            'optimizer.learning_rate': 0.001,
+            'optimizer.model_optimizer_options': None,
+            'optimizer.optimizer': 'Adam',
+            'optimizer.optimizer_options': None,
+            'optimizer.weight_decay': None,
+            'train.archive_every': 10,
+            'train.epochs': 1,
+            'train.eval_during_train': True,
+            'train.eval_only': False,
+            'train.quit_on_bad_values': True,
+            'train.save_on_best': 'losses.classifier',
+            'train.save_on_highest': None,
+            'train.save_on_lowest': None,
+            'train.test_mode': 'test',
+            'train.train_mode': 'train'
+        })
+    args_data = {
+        'source': 'CIFAR10',
+        'batch_size': 128,
+        'n_workers': 4,
+        'skip_last_batch': False,
+        'inputs': {
+            'inputs': 'images'
+        },
+        'copy_to_local': True,
+        'data_args': {},
+        'shuffle': True
+    }
+    args_train = {
+        'epochs': 1,
+        'archive_every': 10,
+        'quit_on_bad_values': True,
+        'save_on_best': 'losses.classifier',
+        'save_on_lowest': None,
+        'save_on_highest': None,
+        'eval_during_train': True,
+        'train_mode': 'train',
+        'test_mode': 'test',
+        'eval_only': False
+    }
+    args_optimizer = {
+        'optimizer': 'Adam',
+        'learning_rate': 0.001,
+        'weight_decay': {},
+        'clipping': {},
+        'optimizer_options': {},
+        'model_optimizer_options': {}
+    }
+
     model = ImageClassification()
     # args = setup_cortex(model=model)
     config.set_config()
