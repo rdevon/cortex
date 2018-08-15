ce78247113c3b2b3f9808a4a9b28254930ac8418
joeljpoulin <joel.poulin@polymtl.ca>
2018-08-15 11:54:16 -0400

    Checkpoint before recloning after repo renaming

--- tests/built_ins/networks/test_base_network.py
+++ tests/built_ins/networks/test_base_network.py
@@ -1,9 +1,7 @@
-from cortex.built_ins.networks.base_network import BaseNet
 from torch import nn
 
 
-def test_base_net():
-    base_net = BaseNet()
-    assert isinstance(base_net.models, nn.Sequential)
-    assert base_net.output_nonlinearity is None
-    assert isinstance(base_net.layer_nonlinearity, nn.ReLU)
+def test_base_net(base_net_model):
+    assert isinstance(base_net_model.models, nn.Sequential)
+    assert base_net_model.output_nonlinearity is None
+    assert isinstance(base_net_model.layer_nonlinearity, nn.ReLU)
