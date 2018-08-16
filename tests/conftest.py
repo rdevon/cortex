5175a425765d453968c7aaaa33d22cc85aeac875
joeljpoulin <joel.poulin@polymtl.ca>
2018-08-16 15:57:57 -0400

    Cleans fixtures structure.

--- tests/conftest.py
+++ tests/conftest.py
@@ -264,3 +264,20 @@ def classifier_modified():
             self.nets.classifier = classifier
 
     return ClassifierModified()
+
+@pytest.fixture
+def base_net_model():
+    from cortex.built_ins.networks.base_network import BaseNet
+    return BaseNet()
+
+
+@pytest.fixture
+def simple_tensor():
+    import torch
+    return torch.Tensor([1., 2., 3.])
+
+
+@pytest.fixture
+def nonlinearity():
+    return dict(
+        sigmoid='sigmoid', tanh='tanh', relu='ReLU', leakyrelu='LeakyReLU')
