d7ba79369755c7a037a51e72a12e2c36ede18d5b
joeljpoulin <joel.poulin@polymtl.ca>
2018-08-22 16:26:59 -0400

    Adds basic build test for ImageClassification.

---
+++ tests/built_ins/models/test_classifier.py
@@ -0,0 +1,29 @@
+from cortex.built_ins.models.classifier import ImageClassification
+from cortex._lib import (data, exp, setup_experiment)
+
+
+def test_image_classification(args):
+    """
+    NOTE: Building with default arguments image_classification.build()
+          fails.
+    TODO: Assert that the model has the right layers and hyperparameters.
+    Args:
+        args (@pytest.fixture): Namespace
+
+    Asserts: model.defaults are the one wanted and the
+             image_classification has the correct layers.
+
+    """
+    image_classification = ImageClassification()
+    image_classification, reload_nets = setup_experiment(
+        args, model=image_classification)
+    data.setup(**exp.ARGS['data'])
+    image_classification.reload_nets(reload_nets)
+
+    expected_defaults = dict(
+        data=dict(batch_size=128, inputs=dict(inputs='images')),
+        optimizer=dict(optimizer='Adam', learning_rate=1e-3),
+        train=dict(epochs=200, save_on_best='losses.classifier'))
+
+    image_classification.build()
+    assert image_classification.defaults == expected_defaults
