23051ac3dbb53d2aa4ee3820ae6ab05a802a45dd
Business Cat <devonh@Devons-MBP.guest.corp.microsoft.com>
2018-08-29 14:21:07 -0400

    visdom 1.8 is buggy

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='cortex',
       packages=packages,
       install_requires=[
           'imageio', 'matplotlib', 'progressbar2', 'scipy', 'sklearn',
-          'torchvision', 'visdom', 'pyyaml', 'sphinxcontrib-napoleon'],
+          'torchvision', 'visdom==0.1.7', 'pyyaml', 'sphinxcontrib-napoleon'],
       entry_points={
           'console_scripts': [
               'cortex=cortex.main:run']
