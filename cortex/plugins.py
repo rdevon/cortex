799bfa399328a848d2622843d5ecf968e7a3c70a
Dmitriy Serdyuk <serdyuk.dmitriy@gmail.com>
2018-09-12 21:05:48 -0400

    Fortmat code

--- cortex/plugins.py
+++ cortex/plugins.py
@@ -1,6 +1,4 @@
-'''Module for plugins
-
-'''
+"""Module for plugins"""
 
 import logging
 from os import path
@@ -24,17 +22,16 @@ logger = logging.getLogger('cortex.plugins')
 
 
 class DatasetPlugin(DatasetPluginBase):
-    '''Basic plugin class for datasets into cortex
+    """Basic plugin class for datasets into cortex.
 
     Attributes:
         sources: list of dataset string names that this plugin will support.
 
-    '''
+    """
     sources = []
 
     def copy_to_local_path(self, from_path: str) -> str:
-
-        ''' Copies data to a local path.
+        """ Copies data to a local path.
 
         Path is set in the .cortex.yml file. This can be set up through
         `cortex setup`.
@@ -42,7 +39,7 @@ class DatasetPlugin(DatasetPluginBase):
         Args:
             from_path: The path to the data to be copied.
 
-        '''
+        """
         if from_path.endswith('/'):
             from_path = from_path[:-1]
         basename = path.basename(from_path)
@@ -53,7 +50,7 @@ class DatasetPlugin(DatasetPluginBase):
                 '`{}` not found in {} data_paths'
                 .format(local_path, _config_name))
         to_path = path.join(local_path, basename)
-        if ((not path.exists(to_path)) and path.exists(from_path)):
+        if (not path.exists(to_path)) and path.exists(from_path):
 
             logger.info('Copying dataset {} from {} to {} directory.... '
                         '(This may take time)'
@@ -69,7 +66,7 @@ class DatasetPlugin(DatasetPluginBase):
         return to_path
 
     def add_dataset(self, mode: str, dataset: Dataset):
-        '''Adds a dataset to the plugin.
+        """Adds a dataset to the plugin.
 
         Any dataset added in this way will be used in the training or testing
         loops, depending on the mode specified.
@@ -79,14 +76,14 @@ class DatasetPlugin(DatasetPluginBase):
                 `train` and `test` are highly recommended.
             dataset: The dataset object.
 
-        '''
+        """
         if mode in self._datasets:
             raise KeyError(
                 '`{}` already added to datasets in entrypoint'.format(mode))
         self._datasets[mode] = dataset
 
     def get_path(self, source: str):
-        '''Get's the path to a source.
+        """Get's the path to a source.
 
         This is derived from config.yaml file.
 
@@ -96,7 +93,7 @@ class DatasetPlugin(DatasetPluginBase):
         Returns:
             The path to the dataset.
 
-        '''
+        """
         p = CONFIG.data_paths.get(source)
         if p is None:
             raise KeyError(
@@ -104,7 +101,7 @@ class DatasetPlugin(DatasetPluginBase):
         return p
 
     def set_input_names(self, input_names):
-        '''Sets the names of the elements of the dataset.
+        """Sets the names of the elements of the dataset.
 
         For use downstream in models.
 
@@ -112,21 +109,21 @@ class DatasetPlugin(DatasetPluginBase):
             input_names (:obj:`list` of :obj:`str`): The input names.
                 Should be the same size as the output of the dataset iterator.
 
-        '''
+        """
         self._input_names = input_names
 
     def set_dims(self, **kwargs):
-        ''' Sets the dimenisions of the data
+        """Sets the dimensions of the data.
 
         Args:
             **kwargs: a dictionary of dimension keys and ints.
 
-        '''
+        """
         for k, v in kwargs.items():
             self._dims[k] = v
 
     def set_scale(self, scale):
-        '''Sets the min / max values for the data.
+        """Sets the min / max values for the data.
 
         Note:
             This will probably be removed. It doesn't even function right now.
@@ -134,11 +131,11 @@ class DatasetPlugin(DatasetPluginBase):
         Args:
             scale (:obj:`tuple` of :obj:`float`): min/max pair.
 
-        '''
+        """
         self._scale = scale
 
     def make_indexing(self, C):
-        '''Makes an indexing dataset.
+        """Makes an indexing dataset.
 
         Index comes in as the last element of the batch.
 
@@ -148,7 +145,7 @@ class DatasetPlugin(DatasetPluginBase):
         Returns:
             Wrapped data.Dataset class.
 
-        '''
+        """
 
         class IndexingDataset(C):
             def __getitem__(self, index):
@@ -159,7 +156,7 @@ class DatasetPlugin(DatasetPluginBase):
 
 
 class ModelPlugin(ModelPluginBase):
-    '''Module plugin.
+    """Module plugin.
 
     Attributes:
         plugin_name (str): Name of the plugin.
@@ -167,7 +164,7 @@ class ModelPlugin(ModelPluginBase):
         train_defaults (:obj:`dict`): Train defaults.
         optimizer_defaults (:obj:`dict`): Optimizer defaults.
 
-    '''
+    """
     _protected = ['description']
     _required = []
     _optional = ['setup']
@@ -175,7 +172,7 @@ class ModelPlugin(ModelPluginBase):
     defaults = {}
 
     def build(self, *args, **kwargs):
-        '''Builds the neural networks.
+        """Builds the neural networks.
 
         The the model is to build something, this needs to be overridden.
 
@@ -183,64 +180,64 @@ class ModelPlugin(ModelPluginBase):
             *args: Inputs to be passed to the function.
             **kwargs: Hyperparameters to be passed to the function
 
-        '''
+        """
         raise NotImplementedError(
             '`build` is not implemented for model class {}'
             .format(self.__class__.__name__))
 
     def routine(self, *args, **kwargs):
-        '''Derives losses and results.
+        """Derives losses and results.
 
-            The the model is to train something, this needs to be
-            overridden.
+        The the model is to train something, this needs to be
+        overridden.
 
-            Args:
-                *args: Inputs to be passed to the function.
-                **kwargs: Hyperparameters to be passed to the function
+        Args:
+            *args: Inputs to be passed to the function.
+            **kwargs: Hyperparameters to be passed to the function
 
-            '''
+        """
         raise NotImplementedError(
             '`routine` is not implemented for model class {}'
             .format(self.__class__.__name__))
 
     def visualize(self, *args, **kwargs):
-        '''Visualizes.
+        """Visualizes.
 
-            The the model is to visualize something, this needs to be
-            overridden.
+        The the model is to visualize something, this needs to be
+        overridden.
 
-            Args:
-                *args: Inputs to be passed to the function.
-                **kwargs: Hyperparameters to be passed to the function
+        Args:
+            *args: Inputs to be passed to the function.
+            **kwargs: Hyperparameters to be passed to the function
 
-            '''
+        """
         pass
 
     def train_step(self):
-        '''Makes a training step.
+        """Makes a training step.
 
         This can be overridden to change the behavior at each training step.
 
-        '''
+        """
         self.data.next()
         self.routine(auto_input=True)
         self.optimizer_step()
 
     def eval_step(self):
-        '''Makes an evaluation step.
+        """Makes an evaluation step.
 
         This can be overridden to change the behavior of each evaluation step.
 
-        '''
+        """
         self.data.next()
         self.routine(auto_input=True)
 
     def optimizer_step(self):
-        '''Makes a step of the optimizers for which losses are defined.
+        """Makes a step of the optimizers for which losses are defined.
 
         This can be overridden to change the behavior of the optimizer.
 
-        '''
+        """
         keys = self.losses.keys()
 
         for i, k in enumerate(keys):
@@ -254,11 +251,11 @@ class ModelPlugin(ModelPluginBase):
                 optimizer.step()
 
     def train_loop(self):
-        '''The training loop.
+        """The training loop.
 
         This can be overridden to change the behavior of the training loop.
 
-        '''
+        """
 
         try:
             while True:
@@ -268,11 +265,11 @@ class ModelPlugin(ModelPluginBase):
             pass
 
     def eval_loop(self):
-        '''The evaluation loop.
+        """The evaluation loop.
 
         This can be overridden to change the behavior of the evaluation loop.
 
-        '''
+        """
 
         try:
             while True:
@@ -282,7 +279,7 @@ class ModelPlugin(ModelPluginBase):
             pass
 
     def get_dims(self, *queries):
-        '''Gets dimensions of inputs.
+        """Gets dimensions of inputs.
 
         Args:
             *queries: Variables to get dimensions of .
@@ -290,60 +287,60 @@ class ModelPlugin(ModelPluginBase):
         Returns:
             Dimensions of the variables.
 
-        '''
+        """
         return self._data.get_dims(*queries)
 
     def add_noise(self, key, dist=None, size=None, **kwargs):
-        '''Adds a noise variable to the model.
+        """Adds a noise variable to the model.
 
         Args:
             key (str): Name of the noise variable.
             dist (str): Noise distribution.
             size (int): Size of the noise.
             **kwargs: keyword arguments for noise distribution.
-        '''
+        """
         self._data.add_noise(key, dist=dist, size=size, **kwargs)
 
     def add_image(self, *args, **kwargs):
-        '''Adds image for visualization.
+        """Adds image for visualization.
 
         Args:
             *args: TODO
             **kwargs: TODO
 
-        '''
+        """
         self._viz.add_image(*args, **kwargs)
 
     def add_histogram(self, *args, **kwargs):
-        '''Adds histogram for visualizaiton.
+        """Adds histogram for visualizaiton.
 
         Args:
             *args: TODO
             **kwargs: TODO
 
-        '''
+        """
         self._viz.add_histogram(*args, **kwargs)
 
     def add_scatter(self, *args, **kwargs):
-        '''Adds a scatter plot to visualization.
+        """Adds a scatter plot to visualization.
 
         Args:
             *args: TODO
             **kwargs: TODO
 
-        '''
+        """
         self._viz.add_scatter(*args, **kwargs)
 
 
 def register_plugin(plugin):
-    '''Registers a plugin into cortex
+    """Registers a plugin into cortex
 
     Args:
         plugin: TODO
 
     Returns:
 
-    '''
+    """
 
     if issubclass(plugin, ModelPlugin):
         register_model(plugin)
